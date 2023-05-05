5f53f084256e88ed1ee4d8812670425e2e0cad95
Therese Cecilia Johansson <Theresececilias@gmail.com>
2023-05-05 11:39:08 +0200

    add basic structure + front page

---
+++ gamesData.js
@@ -0,0 +1,40 @@
+const games = [
+  {
+    id: '1',
+    name: 'random game',
+  },
+  {
+    id: '2',
+    name: 'never have i ever',
+  },
+   {
+    id: '3',
+    name: 'no wrong answers',
+  },
+   {
+    id: '4',
+    name: 'cards against humanity',
+  },
+   {
+    id: '5',
+    name: 'dealbreaker',
+  },
+   {
+    id: '6',
+    name: 'random game',
+  },
+  {
+    id: '7',
+    name: 'would you rather',
+  },
+  {
+    id: '8',
+    name: 'icebreaker questions',
+  },
+  {
+    id: '9',
+    name: 'create game',
+  }
+];
+
+export default games;
