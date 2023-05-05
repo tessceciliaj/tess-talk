15643b887f72342d415a1207e9e1a77bc03e8ee9
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-05-05 13:35:19 +0200

    Revert "add .next folder for build"
    This reverts commit 0c74d1e324782297c99bdf435063d06d3d879148.

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST={pages:{"/_app":["static/media/f0154a1cca127a0d-s.p.woff2"],"/":["static/media/f0154a1cca127a0d-s.p.woff2"]},app:{},appUsingSizeAdjust:!1,pagesUsingSizeAdjust:!0};
