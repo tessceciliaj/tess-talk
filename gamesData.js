8040974b87801fd8bda0931b6c8d4b0f9f688162
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-05-23 14:10:59 +0200

    add gamesData, 'no wrong answers', 'bonding' couples

--- gamesData.js
+++ gamesData.js
@@ -8,6 +8,32 @@ const games = [
     id: '3',
     name: 'no wrong answers',
     link: '/dealbreaker',
+    cards: [
+      "Well it finally happened, you're the Piano Man. Which song are you singing us tonight?",
+      "You just got accepted to Hogwarts! Why are you immediately expelled?",
+      "If you could have a conversation with any fictional character, who would you choose and why?",
+      "If you were stranded on a deserted island and could only bring three items with you, what would they be and why?",
+      "If you could create a new holiday, what would it be called and how would it be celebrated?",
+      "You have the power to travel back in time and witness any historical event. Which event would you choose to experience and why?",
+      "If you were an ice cream flavor, what would you be called and what ingredients would you have?",
+      "Imagine you discover a secret passageway in your house that leads to a hidden room. What do you find inside?",
+      "If you could speak fluently in any language other than your native tongue, which language would you choose and why?",
+      "Imagine you could be any mythical creature for a day. What creature would you choose to be and how would you spend your day?",
+      "What famous villian (fictional or historic) would make the best prom date?",
+      "If you could have a personal mascot that follows you around everywhere, what/who would it be and why?",
+      "If you could replace one body part with a super-powered bionic version, which body part would you choose and what special abilities would it have?",
+      "If you could invent a new flavor of potato chips that would make the headlines, what outrageous flavor combination would you create?",
+      "If you could create a wacky holiday tradition, what would it be and how would people celebrate it?",
+      "If you woke up one day with the ability to understand and speak the language of insects, what funny conversations do you imagine having with them?",
+      "If you were a superhero with a completely useless superpower, what would your power be and how would you try to save the day?",
+      "Which historical figure probably had the best butt?",
+      "Time for a round of shots! But the bar only has condiments left. What condiment are you throwin' back?",
+      "When it's all said and done, what's the sexiest ice cream flavor?",
+      "Let's dance! Choose a historical figure to be your dance partner on a reality TV show, who would you pick and what dance style would you perform together?",
+      "If you could have a personal theme song that played whenever you entered a room, what song would you choose and why?",
+      "If you could turn any mundane task into an extreme sport, what task would you choose and what adrenaline-pumping elements would you add to it?",
+
+    ]
   },
   {
     id: '4',
@@ -63,6 +89,7 @@ const games = [
       'Which food could you eat all year long?',
       'Are you a morning person or a night owl?',
       'Which art do you like the most?',
+      "If you were stranded on a deserted island and could only bring three items with you, what would they be and why?"
     ],
     friendCards: [
       'What quality do you value the most in a friend?',
@@ -108,6 +135,16 @@ const games = [
       'If you are hurt, would you rather be left alone or be hugged tight?',
       'How would you choose to relax after a tiring day?',
       'What is the one thing you always wanted as a child but did not get?',
+      "What is your favorite memory of us as a couple, and why does it stand out to you?",
+      "If we could travel anywhere in the world together, where would you want to go and why?",
+      "What is one thing you appreciate about our relationship that you might not have expected when we first started dating?",
+      "If you could choose one activity for us to do together regularly, what would it be and why?",
+      "What is one personal goal you have for yourself, and how can I support you in achieving it?",
+      "What is your love language, and how can we better express love and affection to each other in ways that resonate with our individual needs?",
+      "What is something new or adventurous that you would like to try as a couple?",
+      "What are three qualities or characteristics you admire in me, and how do they contribute to our relationship?",
+      "How do you envision our future together, and what are some goals or dreams you have for us as a couple?",
+      "What is one thing that you believe sets our relationship apart from others, and why is it significant to you?"
     ],
   },
   {
